e98c67e9c0b77f6b5c3da24319e8d369159cbf0b
kihak12 <60700265+kihak12@users.noreply.github.com>
2020-04-21 14:52:40 +0200

    change

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/UserInfo.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/UserInfo.java
@@ -1,26 +1,17 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
-import android.content.Context;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toolbar;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.openclassrooms.entrevoisins.R;
 
 import butterknife.BindView;
 
 
 public class UserInfo extends AppCompatActivity {
-
-    @BindView(R.id.item_avatar)
-    public ImageView mAvatarUser;
-
-    public String mAvatar;
 
 
     @Override
@@ -30,14 +21,7 @@ public class UserInfo extends AppCompatActivity {
 
         Toolbar returnToMainMenu = (Toolbar) findViewById(R.id.returnToMainMenu);
         ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
 
-        mAvatar = randomImage();
-        Glide.with(this).load(mAvatar).into(mAvatarUser);
     }
 
-
-    String randomImage() {
-        return "https://i.pravatar.cc/150?u="+ System.currentTimeMillis();
-    }
 }
