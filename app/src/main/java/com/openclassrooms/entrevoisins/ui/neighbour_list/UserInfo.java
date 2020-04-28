f7c8f8c4c6700808e863e57b97ad4d5d448a1f0e
kihak12 <60700265+kihak12@users.noreply.github.com>
2020-04-28 15:49:05 +0200

    change

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/UserInfo.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/UserInfo.java
@@ -1,16 +1,24 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toolbar;
 
 import com.openclassrooms.entrevoisins.R;
 
+import butterknife.BindView;
 
 
 public class UserInfo extends AppCompatActivity {
+
+
+    private ImageButton mainMenu;
 
 
     @Override
@@ -20,7 +28,18 @@ public class UserInfo extends AppCompatActivity {
 
         Toolbar returnToMainMenu = (Toolbar) findViewById(R.id.returnToMainMenu);
         ActionBar ab = getSupportActionBar();
+        ab.hide();
 
+        this.mainMenu = findViewById(R.id.mainMenu);
+
+        mainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainReturn = new Intent(getApplicationContext(), ListNeighbourActivity.class);
+                startActivity(mainReturn);
+                finish();
+            }
+        });
 
     }
 
