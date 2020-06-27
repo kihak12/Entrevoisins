abc75da23119819407164f8aee84fea232a4ed92
kihak12 <60700265+kihak12@users.noreply.github.com>
2020-06-27 16:33:04 +0200

    change

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # OpenClassrooms
 
-Ouvrir le projet dans Android Studio
+1-Afin de télécharger le projet sur Github, cliquer sur "Clone", puis sur "Download ZIP".
 
-Afin d'executer et compiler ce code, une fois le "build Gradle" terminé, créer une machine virtuel depuis le menu "Tools" > "AVD Manager".
+2-Extraire le dossier de l'archive télécharger.
+
+3-Puis ouvrez "Android Studio".
+
+4-Cliquez sur "Open an existing Android Studio project" puis sélectionné le projet télécharger.
+
+5-Afin d'executer et compiler ce code, une fois le "build Gradle" terminé, créer une machine virtuel depuis le menu "Tools" > "AVD Manager".
 
 Puis executé "app" avec la machine virtuel sélectionné.
--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -1,25 +1,40 @@
 
 package com.openclassrooms.entrevoisins.neighbour_list;
 
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.app.INotificationSideChannel;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
 import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 
@@ -60,11 +75,50 @@ public class NeighboursListTest {
     @Test
     public void myNeighboursList_deleteAction_shouldRemoveItem() {
         // Given : We remove the element at position 2
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
-        // When perform a click on a delete icon
-        onView(ViewMatchers.withId(R.id.list_neighbours))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(1, new DeleteViewAction()));
-        // Then : the number of element is 11
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT - 1));
+        onView(allOf(withId(R.id.item_list_delete_button),
+                childAtPosition(
+                        childAtPosition(withId
+                                        (R.id.list_neighbours),
+                                0),
+                        2),
+                isDisplayed()))
+                .perform(click())
+                .check(withItemCount(ITEMS_COUNT - 1));
+        // Then : the number of element is 11;
     }
+
+    @Test
+    public void listNeighbourActivityTest() {
+        onView(allOf(withId(R.id.item_list_name),
+                childAtPosition(
+                        childAtPosition(
+                                withId(R.id.list_neighbours),
+                                0),
+                        1),
+                isDisplayed())).perform(click());
+
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+
 }
+
